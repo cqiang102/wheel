76c11b64d20f173c15d4ef359f67277f6d6d4b3f
cqiang102 <2375622526@qq.com>
2019-12-14 23:57:51 +0800

    water

--- spring-cq/src/main/java/cn/lacia/wheel/spring/cq/LaciaSpringApplication.java
+++ spring-cq/src/main/java/cn/lacia/wheel/spring/cq/LaciaSpringApplication.java
@@ -9,8 +9,12 @@ import cn.lacia.wheel.spring.cq.core.AnnontaionContextApplication;
 public class LaciaSpringApplication {
     public static void run(Class clazz,String[] args){
 
+        // 开启 TomCat 通过 往 TomCat 注册一个 Servlet 从servlet 开启 AnnontaionContextApplication，运行 Spring 项目
+        // 暂时先直接启动
         AnnontaionContextApplication annontaionContextApplication = new AnnontaionContextApplication();
+        annontaionContextApplication.setScanClass(clazz);
 
+        annontaionContextApplication.run();
 
     }
 
--- spring-cq/src/main/java/cn/lacia/wheel/spring/cq/core/AnnontaionContextApplication.java
+++ spring-cq/src/main/java/cn/lacia/wheel/spring/cq/core/AnnontaionContextApplication.java
@@ -1,15 +1,35 @@
 package cn.lacia.wheel.spring.cq.core;
 
+import cn.lacia.wheel.spring.cq.annotation.LaciaApplication;
+
+import java.lang.annotation.Annotation;
+
 /**
  * @author 你是电脑
  * @create 2019/11/28 - 13:44
  */
 public class AnnontaionContextApplication {
+
+    private Class clazz;
+
+    public void setScanClass(Class clazz){
+        this.clazz = clazz;
+    }
     /**
      * 根据 Class 上的注解获得包扫描路径
      * @param clazz
      */
     private void getScannerUrl(Class clazz) {
+        Annotation annotation = clazz.getAnnotation(LaciaApplication.class);
+        System.out.println(annotation);
+    }
 
+    public void run() {
+
+        // 扫描包
+        getScannerUrl(this.clazz);
+        // 实例化
+        // 依赖注入
+        // 注册 HandlerMapping
     }
 }
